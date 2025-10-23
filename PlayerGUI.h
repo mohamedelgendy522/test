1484e37a9d3a5d603bf11ba2c6124cada6a6b79d
Mohamed Mahmoud Ahmed Mahmoud ELGendy <mm6920@o365.fayoum.edu.eg>
2025-10-23 07:18:29 +0300

    Add PlayerGUI class for audio player interface

---
+++ PlayerGUI.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <JuceHeader.h>
+#include "PlayerAudio.h"
+class PlayerGUI : public juce::Component,
+    public juce::Button::Listener,
+    public juce::Slider::Listener, public juce::AudioSource
+
+{
+public:
+    PlayerGUI();
+    ~PlayerGUI() override;
+
+    void resized() override;
+
+    void paint(juce::Graphics& g) override;
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
+    void releaseResources();
+
+private:
+    PlayerAudio playerAudio;
+
+    // GUI elements
+    juce::TextButton loadButton{ "Load File" };
+    juce::TextButton restartButton{ "Restart" };
+    juce::TextButton stopButton{ "Stop" };
+    juce::Slider volumeSlider;
+
+    std::unique_ptr<juce::FileChooser> fileChooser;
+
+    // Event handlers
+    void buttonClicked(juce::Button* button) override;
+    void sliderValueChanged(juce::Slider* slider) override;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGUI)
+
+};
